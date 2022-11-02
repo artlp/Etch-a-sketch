eae703af8c642a15db85c24b4920011da2581928
Artur Lipskii <falcon.arthur@gmail.com>
2022-11-02 11:04:03 +0200

    initial

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
 const canvasSize = document.querySelector('#canvasSize');
+const canvasSize2 = document.querySelector('#canvasSize2');
 const divCenter = document.querySelector('.divcenter');
 const btnReset = document.querySelector('#resetButton');
-const colorPicker = document.querySelector('#colorPicker');
+// const colorPicker = document.querySelector('#colorPicker');
+const colorPicker = document.querySelector('#hiddencolor');
 let pixels = document.querySelectorAll(".grid");
 let color = colorPicker.value;
 const random = document.querySelector('#randomColor');
 const toggleGrid = document.querySelector('#toggleGrid');
+const displaySize = document.querySelector('#gridSize');
+const btnGridSize = document.querySelector('.infodisplay');
 
 colorPicker.addEventListener('input', changeColor);
 function changeColor() {
     color = colorPicker.value;
+    document.querySelector(".colordisplay").style.backgroundColor = colorPicker.value;
 }
+
+btnGridSize.addEventListener('click', () => {
+    canvasSize.classList.toggle("hidden");
+    canvasSize2.classList.toggle("hidden");
+})
 
 toggleGrid.addEventListener('change', () => {
     pixels.forEach((e) => {
@@ -19,8 +29,15 @@ toggleGrid.addEventListener('change', () => {
     
 })
 
+displaySize.innerText = +canvasSize.value;
+displaySize.innerText = +canvasSize2.value;
 
-let gridSize = +canvasSize.value;
+random.addEventListener('click', () => {
+    document.querySelector(".colordisplay").classList.toggle("rainbow");
+
+})
+
+// let gridSize = sizeController[0].endRotation;
 let draw = 0;
 
 function randomColor() {
@@ -32,10 +49,16 @@ function randomColor() {
 
 generateGrid()
 canvasSize.addEventListener('change', generateGrid);
+canvasSize2.addEventListener('change', generateGrid);
 
 function generateGrid() {
     divCenter.innerHTML = '';
     gridSize = +canvasSize.value;
+    displaySize.innerText = +canvasSize.value;
+    gridSize = +canvasSize2.value;
+    displaySize.innerText = +canvasSize2.value;
+
+    // gridSize = Math.round(sizeController[0].endRotation);
     divCenter.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
     divCenter.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
     for (let i = 1; i <= gridSize * gridSize; i++) {
@@ -74,6 +97,7 @@ function paint(e) {
         e.target.style.backgroundColor = `${color}`;
     } else if (e.type === 'mousedown') {
         if (random.checked === true) {
+            // document.querySelector(".colordisplay").style.backgroundImage = "linear-gradient(to right, red,orange,yellow,green,blue,indigo,violet)";
             color = randomColor();
         }
         e.target.style.backgroundColor = `${color}`;
@@ -93,3 +117,4 @@ btnReset.addEventListener('click', () => {
         e.style.backgroundColor = "white";
     })
 })
+
